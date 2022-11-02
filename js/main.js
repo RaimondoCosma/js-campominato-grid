d25751d5656df2889ff986954f10fbe1d096c8f4
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-02 14:18:07 +0100

    Aggiunge ciclo for per creare box dinamicamente

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Dichiaro la variabile relativa al box__container
+const boxContainer = document.getElementById('box__container');
+// Inizializzo un ciclo for per creare i miei box dinamicamente dentro il box__container
+for ( let i = 1; i <= 100; i++ ){
+    let boxItem = document.createElement("div");
+    boxItem.innerHTML = i;
+    boxItem.classList.add('box__item');
+    boxContainer.append(boxItem);
+}
