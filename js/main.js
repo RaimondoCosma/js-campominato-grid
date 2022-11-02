41fc4dedeb8f903a024f58d38ad8d6c30d665377
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-02 16:57:29 +0100

    Aggiunge funzione nel file javascript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 "use strict";
 
+/*-----------------------
+    FUNCTIONS
+-----------------------*/
+function boxCreation (howManyBox, howManyColumn) {
+     // Svuoto il contenuto prima di inserire i box per renderlo più dinamico al cambio di difficoltà
+     boxContainer.innerHTML = "";
+     // Inizializzo un ciclo for per creare i miei box dinamicamente dentro il box__container
+     for ( let i = 1; i <= howManyBox; i++ ){
+         let boxItem = document.createElement("div");
+         boxItem.innerHTML = i;
+         boxItem.classList.add('box__item',`box__item${howManyColumn}`);
+         boxContainer.append(boxItem);
+         // Aggiungo evento click al box della cella
+         boxItem.addEventListener("click", function(){
+             this.classList.toggle("bg-change");
+             console.log(`Hai cliccato la casella numero: ${this.innerHTML}`);
+         })
+     }
+}
+/*-----------------------
+    MAIN
+-----------------------*/
 // Dichiaro la variabile relativa al box__container
 const boxContainer = document.getElementById('box__container');
 
 // Dichiaro la variabile relativa al bottone play
 const play = document.getElementById('play__btn');
-
 
 // Aggiungo evento al play che mostra la griglia
 play.addEventListener('click', function(){
@@ -16,47 +37,19 @@ play.addEventListener('click', function(){
     if ( difficulties === "Hard" ){
         // Svuoto il contenuto prima di inserire i box per renderlo più dinamico al cambio di difficoltà
         boxContainer.innerHTML = "";
-        // Inizializzo un ciclo for per creare i miei box dinamicamente dentro il box__container
-        for ( let i = 1; i <= 100; i++ ){
-            let boxItem = document.createElement("div");
-            boxItem.innerHTML = i;
-            boxItem.classList.add('box__item');
-            boxContainer.append(boxItem);
-            // Aggiungo evento click al box della cella
-            boxItem.addEventListener("click", function(){
-                this.classList.toggle("bg-change");
-                console.log(`Hai cliccato la casella numero: ${this.innerHTML}`);
-            })
-        }
+        // Richiamo la funzione con i parametri per la difficoltà Difficile
+       boxCreation(100, 10);
+
     } else if ( difficulties === "Normal" ){
         // Svuoto il contenuto prima di inserire i box per renderlo più dinamico al cambio di difficoltà
         boxContainer.innerHTML = "";
-        // Inizializzo un ciclo for per creare i miei box dinamicamente dentro il box__container
-        for ( let i = 1; i <= 81; i++ ){
-            let boxItem = document.createElement("div");
-            boxItem.innerHTML = i;
-            boxItem.classList.add('box__item81');
-            boxContainer.append(boxItem);
-            // Aggiungo evento click al box della cella
-            boxItem.addEventListener("click", function(){
-                this.classList.toggle("bg-change");
-                console.log(`Hai cliccato la casella numero: ${this.innerHTML}`);
-            })
-        }
+        // Richiamo la funzione con i parametri per la difficoltà Normale
+        boxCreation(81, 9);
+
     } else {
         // Svuoto il contenuto prima di inserire i box per renderlo più dinamico al cambio di difficoltà
         boxContainer.innerHTML = "";
-        // Inizializzo un ciclo for per creare i miei box dinamicamente dentro il box__container
-        for ( let i = 1; i <= 49; i++ ){
-            let boxItem = document.createElement("div");
-            boxItem.innerHTML = i;
-            boxItem.classList.add('box__item49');
-            boxContainer.append(boxItem);
-            // Aggiungo evento click al box della cella
-            boxItem.addEventListener("click", function(){
-                this.classList.toggle("bg-change");
-                console.log(`Hai cliccato la casella numero: ${this.innerHTML}`);
-            })
-        }
+        // Richiamo la funzione con i parametri per la difficoltà Facile
+        boxCreation(49, 7);
     }
 })
